ac866b8e8f03fd423fe16f0985adb7873cf90cef
Kellen Roy <kellenroy@gmail.com>
2018-07-17 16:12:40 -0700

    implementing API

--- TriviaGame/SwiftFiles/CategoriesController.swift
+++ TriviaGame/SwiftFiles/CategoriesController.swift
@@ -13,15 +13,14 @@ import SwiftyJSON
 
 class CategoriesController: UIViewController {
     
+    @IBOutlet weak var generalButton: UIButton!
+    @IBOutlet weak var moviesButton: UIButton!
+    @IBOutlet weak var gamesButton: UIButton!
+    @IBOutlet weak var sportsButton: UIButton!
+    @IBOutlet weak var scienceButton: UIButton!
+    @IBOutlet weak var historyButton: UIButton!
+    
 
-    //API Setups
-    //let generalQuestions = "https://opentdb.com/api.php?amount=10&category=9&type=multiple"
-   // let
-    
-    //let userData = try! JSON(data: jsonDATA)
-
-    
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -58,6 +57,30 @@ class CategoriesController: UIViewController {
             print("Transitioning to the Categories View Controller")
         }
         
+    }
+ 
+    @IBAction func generalButtonPressed(_ sender: Any) {
+
+        let generalQuestions = "https://opentdb.com/api.php?amount=10&category=9&type=multiple"
+        
+        Alamofire.request(generalQuestions).validate().responseJSON() { response in
+            switch response.result {
+                case .success:
+                if let value = response.result.value {
+                let json = JSON(value)
+    
+    //setup complete, code under here
+                    
+        // example of JSON searching
+        //let firstName = userData["results"][0]["name"]["first"].stringValue
+                    
+        
+                    
+        }
+        case .failure(let error):
+        print(error)
+        }
+        }
     }
     
 }
